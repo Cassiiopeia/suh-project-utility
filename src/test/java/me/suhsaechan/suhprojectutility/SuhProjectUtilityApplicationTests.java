7d1c28222f7c1c73ccd6ddfab06c0cecdaa62637
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-02-01 16:30:24 +0900

    init: project init - basic files

---
+++ src/test/java/me/suhsaechan/suhprojectutility/SuhProjectUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhprojectutility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhProjectUtilityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
