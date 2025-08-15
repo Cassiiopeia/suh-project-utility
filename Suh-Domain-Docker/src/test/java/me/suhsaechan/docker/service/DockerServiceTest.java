0ff3f5cac993443745d17a1383b09d6f0792e0a0
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-08-15 17:57:45 +0900

    프로젝트 테스트코드 정상화 및 https NEXUS 연결 : fix : WEB 모듈 의존성 각 모듈에 추가(테스트시에만) application메인함수 연결 https://github.com/Cassiiopeia/suh-project-utility/issues/64

--- Suh-Domain-Docker/src/test/java/me/suhsaechan/docker/service/DockerServiceTest.java
+++ Suh-Domain-Docker/src/test/java/me/suhsaechan/docker/service/DockerServiceTest.java
@@ -2,12 +2,13 @@ package me.suhsaechan.docker.service;
 
 import static me.suhsaechan.suhlogger.util.SuhLogger.lineLog;
 
+import me.suhsaechan.web.SuhProjectUtilityApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = SuhProjectUtilityApplication.class)
 @ActiveProfiles("dev")
 class DockerServiceTest {
    @Autowired
--- Suh-Domain-Module/src/test/java/me/suhsaechan/module/service/ModuleVersionServiceTest.java
+++ Suh-Domain-Module/src/test/java/me/suhsaechan/module/service/ModuleVersionServiceTest.java
@@ -16,8 +16,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import me.suhsaechan.web.SuhProjectUtilityApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = SuhProjectUtilityApplication.class)
 @ActiveProfiles("dev")
 //@Transactional
 @Slf4j
